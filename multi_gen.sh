29cab3be51b15807e21906186a11a51a0f6cb2a7
Ulric Qin <ulric.qin@gmail.com>
2014-05-24 13:26:27 +0800

    add multi_gen.sh

---
+++ multi_gen.sh
@@ -0,0 +1,112 @@
+#! /usr/bin/env bash
+
+# duo_yuan_1
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_1
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_2
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_3
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_4
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_5
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_6
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_7
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_8
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_9
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_10
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_11
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_12
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_13
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_14
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_15
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_16
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_17
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_18
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_19
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_1/ji_yi_20
+
+
+# duo_yuan_2
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_1
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_2
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_3
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_4
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_5
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_6
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_7
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_8
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_9
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_10
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_11
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_12
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_13
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_14
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_15
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_16
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_17
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_18
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_2/ji_yi_19
+
+# duo_yuan_3
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_1
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_2
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_3
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_4
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_5
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_6
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_7
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_8
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_9
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_10
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_11
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_12
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_13
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_14
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_15
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_16
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_17
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_18
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_19
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_3/ji_yi_20
+
+# duo_yuan_4
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_1
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_2
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_3
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_4
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_5
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_6
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_7
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_8
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_9
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_10
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_11
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_12
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_13
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_14
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_15
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_16
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_17
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_18
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_4/ji_yi_19
+
+# duo_yuan_5
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_1
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_2
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_3
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_4
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_5
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_6
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_7
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_8
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_9
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_10
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_11
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_12
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_13
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_14
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_15
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_16
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_17
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_18
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_19
+./randgen 5-15 10000 ~/caipiao_data/duo_yuan_5/ji_yi_20
+
+
