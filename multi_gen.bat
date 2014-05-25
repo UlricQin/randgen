a4d63062b5c425c28d02ac884f0b1d7d87348554
Ulric Qin <qinxiaohui@xiaomi.com>
2014-05-25 22:29:28 +0800

    add bat

---
+++ multi_gen.bat
@@ -0,0 +1,70 @@
+:: duo_yuan_1
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_1
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_2
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_3
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_4
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_5
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_6
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_7
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_8
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_9
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_10
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_11
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_12
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_13
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_14
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_15
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_16
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_17
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_18
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_19
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_1\ji_yi_20
+
+:: duo_yuan_2
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_1
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_2
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_3
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_4
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_5
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_6
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_7
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_8
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_9
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_10
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_11
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_12
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_13
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_14
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_15
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_16
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_17
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_18
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_19
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_2\ji_yi_20
+
+:: duo_yuan_3
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_1
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_2
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_3
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_4
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_5
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_6
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_7
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_8
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_9
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_10
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_11
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_12
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_13
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_14
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_15
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_16
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_17
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_18
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_19
+randgen.exe 1-5 2 c:\caipiao_data\duo_yuan_3\ji_yi_20
+
+
+
+
+
